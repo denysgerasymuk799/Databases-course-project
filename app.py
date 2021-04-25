af0f2ec912d8b8f0c6d0669038d057575b013421
Denys Herasymuk <denys.herasymuk@eleks.com>
2021-04-25 20:04:55 +0300

    added flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_main_page():
+    return render_template("index.html")
+
+
+@app.route('/request1', methods=['GET', 'POST'])
+def request_page_action1():
+    return render_template("request1.html")
+
+
+@app.route('/request2', methods=['GET', 'POST'])
+def request_page_action2():
+    return render_template("request2.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
