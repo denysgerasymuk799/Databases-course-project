b4f2a987d7bcd85fd819f849aa2b0643b723a735
Denys Herasymuk <denys.herasymuk@eleks.com>
2021-05-07 12:58:48 +0300

    added new scheme

--- models.py
+++ models.py
@@ -15,3 +15,20 @@ class Agronomist(db.Model):
 
     def __repr__(self):
         return '<Customer %r>' % self.customer_name
+
+
+class Sort(db.Model):
+    sort_id = db.Column(db.Integer, primary_key=True)
+    sort_name = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __repr__(self):
+        return '<Sort %r>' % self.sort_name
+
+
+class Product(db.Model):
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False, unique=True)
+    price = db.Column(db.Numeric(12, 2), nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.product_name
