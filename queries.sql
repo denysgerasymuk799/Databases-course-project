f84f5c74892ed2c4c1c007db19335f4fa836594e
Dranixia <dranixoverlord@gmail.com>
2021-05-22 15:21:59 +0300

    finished queries

--- queries.sql
+++ queries.sql
@@ -1,61 +1,93 @@
--- 1 
+-- 1 Для агронома A знайти усiх споживачiв, яким вiн продавав продукт хоча б N разiв
+-- за вказаний перiод (з дати F по дату T)
 SELECT customer_name FROM Customer WHERE customer_id IN 
 (SELECT customer_id FROM
-(SELECT customer_id, COUNT(customer_id) AS num FROM Ordering WHERE agronomist_id = "A" 
-	AND order_date BETWEEN 'F' AND 'T' GROUP BY customer_id) h 
-WHERE h.num > "N");
+(SELECT customer_id, COUNT(customer_id) AS num FROM Ordering WHERE agronomist_id = 1
+	AND order_date BETWEEN '2010-01-01' AND '2021-01-01' GROUP BY customer_id) h 
+WHERE h.num > 0);
 
---2 
-SELECT product_name FROM Product WHERE product_id IN (SELECT DISTINCT product_id FROM Ordering WHERE customer_id = "C" AND order_date BETWEEN 'F' AND 'T');
+--2 Для споживача С знайти усi продукти, якi вiн придбав за вказаний перiод (з дати F по дату T)
+SELECT product_name FROM Product WHERE product_id IN
+	(SELECT DISTINCT product_id FROM Ordering
+	 WHERE customer_id = 18 AND order_date BETWEEN '2000-01-01' AND '2021-01-01');
 
---3 
+-- example of no such order
+SELECT product_name FROM Product WHERE product_id IN
+	(SELECT DISTINCT product_id FROM Ordering
+	 WHERE customer_id = 10 AND order_date BETWEEN '2000-01-01' AND '2021-01-01');
+
+--3 Для споживача С знайти усiх агрономiв, якi проводили для нього дегустацiю хоча б N разiв
+-- за вказаний перiод (з дати F по дату T)
 SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN
-(SELECT agronomist_id FROM (SELECT agronomist_id, COUNT(agronomist_id) AS num FROM 
-Degustation INNER JOIN Degustation_Customer ON 
-Degustation.degustation_id = Degustation_Customer.degustation_id 
-WHERE Degustation_Customer.customer_id = "C" GROUP BY agronomist_id) h WHERE h.num > "N");
+	(SELECT agronomist_id FROM (SELECT agronomist_id, COUNT(agronomist_id) AS num FROM 
+	Degustation INNER JOIN Degustation_Customer ON 
+	Degustation.degustation_id = Degustation_Customer.degustation_id 
+	WHERE Degustation_Customer.customer_id = 2 AND degustation_date BETWEEN '2000-01-01' AND '2021-01-01' GROUP BY agronomist_id) h
+ WHERE h.num >= 1);
 
---4 
+--4 Для агронома А знайти усiх агрономiв, з якими вiн їздив у вiдрядження протягом
+-- вказаного перiоду (з дати F по дату T)
+SELECT * FROM  Trip_Agronomist
+SELECT * FROM  business_trip
+SELECT * FROM  Agronomist
+
 SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN 
-(SELECT DISTINCT agronomist_id FROM Trip_Agronomist WHERE agronomist_id != "A" AND trip_id IN
-(SELECT trip_id FROM business_trip WHERE trip_date BETWEEN "F" AND "T" AND trip_id IN 
-(SELECT trip_id FROM Trip_Agronomist WHERE agronomist_id = "A")));
+(SELECT DISTINCT agronomist_id FROM Trip_Agronomist WHERE agronomist_id != 2 AND trip_id IN
+(SELECT trip_id FROM business_trip WHERE trip_date BETWEEN '2000-01-01' AND '2021-01-01' AND trip_id IN 
+(SELECT trip_id FROM Trip_Agronomist WHERE agronomist_id = 2)));
  
---5 
+--5 Для споживача С знайти усiх агрономiв, якi продали йому хоча б щось та провели для нього
+-- хоча б одну дегустацiю протягом вказаного перiоду (з дати F по дату T)
 SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN 
-(SELECT agronomist_id FROM Ordering WHERE customer_id = "C" AND order_date BETWEEN "F" AND "T" )
+	(SELECT agronomist_id FROM Ordering
+	 WHERE customer_id = 5 AND order_date BETWEEN '2010-01-01' AND '2021-01-01')
 AND agronomist_id IN
-(SELECT agronomist_id FROM Degustation WHERE degustation_id IN 
-(SELECT * FROM degustation_customer WHERE customer_id = "C")
- AND degustation_date BETWEEN "F" AND "T");
+	(SELECT agronomist_id FROM Degustation WHERE degustation_id IN 
+	(SELECT degustation_id FROM degustation_customer WHERE customer_id = 5)
+ 	AND degustation_date BETWEEN '2010-01-01' AND '2021-01-01');
 
---6 
+--6 Знайти усiх споживачiв, якi купили щонайменше N рiзних продуктiв за вказаний перiод
+-- (з дати F по дату T)
 SELECT customer_name FROM Customer WHERE customer_id IN
 (SELECT customer_id FROM 
- (SELECT customer_id, COUNT(DISTINCT product_id) AS num FROM Ordering WHERE order_date BETWEEN "F" AND "T" GROUP BY customer_id) h
- WHERE h.num > "N");
+ (SELECT customer_id, COUNT(DISTINCT product_id) AS num FROM Ordering INNER JOIN Ordering_Product ON Ordering.order_id = Ordering_Product.order_id 
+  WHERE order_date BETWEEN '2010-01-01' AND '2021-01-01' GROUP BY customer_id) h
+ WHERE h.num > 8);
  
---7 Works
+--7 Знайти усiх агрономiв, якi збирали урожай хоча б N рiзних сортiв коноплi
+-- за вказаний перiод (з дати F по дату T)
  SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN (
- SELECT * FROM (SELECT agronomist_id, COUNT(sort_id) AS num FROM Harvest WHERE harvest_date BETWEEN "F" AND "T" GROUP BY agronomist_id) h
- WHERE h.num > "N");
+ SELECT agronomist_id FROM (SELECT agronomist_id, COUNT(sort_id) AS num FROM Harvest
+				WHERE harvest_date BETWEEN '2010-01-01' AND '2021-01-01' GROUP BY agronomist_id) h
+ WHERE h.num > 1);
  
- --8 Works
-SELECT DISTINCT Degustation.degustation_id FROM Degustation INNER JOIN Degustation_Customer ON 
-Degustation.degustation_id = Degustation_Customer.degustation_id 
-WHERE Degustation_Customer.customer_id = "C" AND Degustation.agronomist_id = "A";
+ --8 Знайти усi спiльнi дегустацiї для споживача С та агронома А
+ -- за вказаний перiод (з дати F по дату T)
+SELECT DISTINCT Degustation.degustation_id FROM Degustation
+INNER JOIN Degustation_Customer
+ON Degustation.degustation_id = Degustation_Customer.degustation_id 
+WHERE Degustation_Customer.customer_id = 1 AND Degustation.agronomist_id = 1
+AND Degustation.degustation_date BETWEEN '2010-01-01' AND '2021-01-01';
 
---9 Works
+--9 Для агронома A та кожного продукту, дегустацiю якого вiн проводив, знайти скiльки разiв
+-- за вказаний перiод (з дати F по дату T) вiн проводив дегустацiю для щонайменше N споживачiв
 SELECT product_id, COUNT(product_id) FROM Degustation WHERE degustation_id IN (
-SELECT * FROM (SELECT Degustation.degustation_id, COUNT(DISTINCT Degustation_Customer.customer_id) AS num FROM Degustation 
+SELECT degustation_id FROM (SELECT Degustation.degustation_id, COUNT(DISTINCT Degustation_Customer.customer_id) AS num FROM Degustation 
 INNER JOIN Degustation_Customer ON 
 Degustation.degustation_id = Degustation_Customer.degustation_id  
-WHERE degustation_date BETWEEN "F" AND "T" AND agronomist_id = "A" GROUP BY degustation_id) h WHERE h.num > "N") GROUP BY product_id;
+WHERE degustation_date BETWEEN '2000-01-01' AND '2021-01-01'
+AND agronomist_id = 1 GROUP BY Degustation.degustation_id) h WHERE h.num > 1) GROUP BY product_id;
 
---10 Not done
+--10 Для споживача С знайти сумарну кiлькiсть вiдгукiв по мiсяцях, якi вiн залишив за вказаний перiод
+-- (з дати F по дату T)
 
+SELECT EXTRACT(YEAR FROM return_date) AS years, EXTRACT (MONTH FROM return_date) AS months, COUNT(return_id) AS TOTALCOUNT 
+FROM Order_Return WHERE return_date BETWEEN '2010-01-01' AND '2021-01-01'
+GROUP BY years, months
+ORDER BY years, months;
 
---11 Works
+--11 Вивести сорти коноплi у порядку спадання середньої кiлькостi вiдряджень, у якi їздили агрономи,
+-- що збирали його урожай хоча б N разiв за вказаний перiод (з дати F по дату T)
 CREATE OR REPLACE VIEW hrvsts AS (SELECT sort_name, f.agronomist_name, harvest_date, 
     CASE 
       WHEN trips IS NULL THEN 0 
@@ -63,34 +95,50 @@ CREATE OR REPLACE VIEW hrvsts AS (SELECT sort_name, f.agronomist_name, harvest_d
     END
     FROM (
     SELECT * FROM (sort s INNER JOIN harvest h ON s.sort_id = h.sort_id) d INNER JOIN agronomist a ON d.agronomist_id = a.agronomist_id 
-WHERE harvest_date BETWEEN 'F' AND 'T' ) f 
+WHERE harvest_date BETWEEN '2010-01-01' AND '2021-01-01' ) f 
     LEFT JOIN 
         (SELECT agronomist_name, COUNT(agronomist_name) trips FROM agronomist a 
      INNER JOIN trip_agronomist ta on a.agronomist_id = ta.agronomist_id GROUP BY agronomist_name) g 
-     on f.agronomist_name = g.agronomist_name )
+     on f.agronomist_name = g.agronomist_name );
 
 SELECT sort_name, CASE
     WHEN sort_name IN 
         (SELECT sort_name FROM hrvsts
          group by (sort_name, agronomist_name)
-         having count(agronomist_name) > 'N') THEN SUM(trips)/COUNT(agronomist_name)
+         having count(agronomist_name) > 1)
+		 THEN SUM(trips)/COUNT(agronomist_name)
   ELSE 0
   END AS av_trips
 FROM hrvsts 
 GROUP BY sort_name
 ORDER BY av_trips DESC
 
---12 Works
-SELECT product_name, CAST(returned AS DECIMAL)/bought * 100 perc FROM
+--12 Вивести продукти, якi були придбанi щонайменше N рiзними споживачами у порядку
+-- спадання вiдсотку повернень за вказаний перiод (з дати F по дату T)
+SELECT product_name, ROUND(CAST(returned AS DECIMAL)/bought * 100, 2) perc FROM
 (SELECT d.product_name, bought, CASE 
    WHEN returned IS NULL THEN 0
    ELSE returned
    END AS returned
 FROM 
-(SELECT product_name, COUNT(product_name) bought FROM ORDERING o INNER JOIN product p ON o.product_id = p.product_id
+(SELECT product_name, COUNT(product_name) bought FROM (ordering o LEFT JOIN ordering_product op ON o.order_id = op.order_id) f 
+LEFT JOIN product p ON f.product_id = p.product_id
 GROUP BY product_name) d LEFT JOIN
-(SELECT product_name, COUNT(product_name) returned FROM (ORDERING o INNER JOIN product p ON o.product_id = p.product_id) f
-RIGHT JOIN order_return orr ON f.order_id = orr.order_id WHERE return_date BETWEEN 'F' AND 'T'
+(SELECT product_name, COUNT(product_name) returned FROM ((SELECT op.order_id, product_id FROM ordering o LEFT JOIN ordering_product op ON o.order_id = op.order_id) f 
+LEFT JOIN product p ON f.product_id = p.product_id) n RIGHT JOIN order_return orr ON n.order_id = orr.order_id  
+WHERE return_date BETWEEN '01/01/2000' AND '01/01/2020'
 GROUP BY product_name) g on d.product_name = g.product_name
-WHERE bought > 'N') h
+WHERE bought >= 2) h
 ORDER BY perc DESC
+
+--13  Для користувача С вивести суми грошей, на які він купив продукти у різних агрономів, у вказаний період (з дати F по дату T)
+
+SELECT SUM(price) FROM Product WHERE product_id IN 
+(SELECT product_id FROM Ordering_Product WHERE order_id IN 
+ (SELECT order_id FROM Ordering WHERE customer_id = 4 AND order_date BETWEEN '2010-01-01' AND '2021-01-01'))
+
+--14 Для агронома А вивести прибуток за вказаний період (з дати F по дату T)
+
+SELECT SUM(price) FROM Product WHERE product_id IN 
+(SELECT product_id FROM Ordering_Product WHERE order_id IN 
+ (SELECT order_id FROM Ordering WHERE agronomist_id = 4 AND order_date BETWEEN '2010-01-01' AND '2021-01-01'))
