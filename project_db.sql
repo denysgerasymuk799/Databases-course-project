8776983041ea4bb84917dd0c53938d401014039d
Denys Herasymuk <denys.herasymuk@eleks.com>
2021-05-24 01:19:24 +0300

    all checked

--- project_db.sql
+++ project_db.sql
@@ -768,9 +768,10 @@ INSERT INTO trip_agronomist VALUES
     (3, 1),
     (10, 5)
 ;
+
 -- Creating indexes
+CREATE INDEX idx_agronom_id ON agronomist (agronomist_id);
 CREATE INDEX idx_agronom_name ON agronomist (agronomist_name);
 CREATE INDEX idx_customer_name ON customer (customer_name);
 CREATE INDEX idx_sort_name ON sort (sort_name);
 CREATE INDEX idx_weight_ord ON ordering (weight);
-CREATE INDEX idx_total_proce_ord ON ordering (total_price);
