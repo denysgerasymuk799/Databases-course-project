14002c9474d72c4ef233054a840a369860557354
nazar1ous <kuspys@ucu.edu.ua>
2021-05-23 05:03:19 +0300

    added requests 3-6

--- app.py
+++ app.py
@@ -149,38 +149,269 @@ def request_page_action2():
 def request_page_action3():
     statement = text("""SELECT customer_id, customer_name FROM customer""")
 
-    rs = db.session.execute(statement).all()
+    customer_values_sample = db.session.execute(statement).all()
     db.session.commit()
     db.session.close()
 
-    print("request_page_action3 -- ", rs)
+    print("request_page_action3", customer_values_sample)
 
-    customer_values_sample = [(1, "Lima"), (3, "Huka")]
-    return render_template("request3.html", customer_lst=customer_values_sample)
+    table_cols = ["agronomist_id", "agronomist_name"]
+    if request.method == 'GET':
+        return render_template("request3.html",
+                               customer_lst=customer_values_sample,
+                               selected_cust_id=1,
+                               selected_from_date="-",
+                               selected_to_date="-",
+                               table_cols=table_cols,
+                               agronomists=[(0, "No result")])
+
+    if request.method == 'POST':
+        # Get a data in json format(similar)
+        data = {
+            "cust_id": int(request.form.get("cust_id")),
+            "deg_num": int(request.form.get("deg_num")),
+            "from_date": request.form.get("from_date"),
+            "to_date": request.form.get("to_date")
+        }
+
+        today = date.today()
+        if data["from_date"] == '':
+            data["from_date"] = '2010-01-01'
+
+        if data["to_date"] == '':
+            data["to_date"] = today.strftime("%Y-%m-%d")
+
+        print("data in request3_handle -- ", data)
+
+        statement = text(
+            """
+            SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN
+(SELECT agronomist_id FROM (SELECT agronomist_id, COUNT(agronomist_id) AS num FROM 
+Degustation INNER JOIN Degustation_Customer ON 
+Degustation.degustation_id = Degustation_Customer.degustation_id 
+WHERE Degustation_Customer.customer_id = :cust_id GROUP BY agronomist_id) h WHERE h.num > :deg_num
+AND order_date BETWEEN :from_date AND :to_date);
+            """
+        )
+
+        rs = db.session.execute(statement, {
+            "cust_id": data["cust_id"],
+            "deg_num": data["deg_num"],
+            "from_date": data["from_date"],
+            "to_date": data["to_date"]
+        })
+        db.session.commit()
+        db.session.close()
+
+        results = rs.fetchall()
+        print("request3_handle results -- ", results)
+
+        return render_template("request3.html",
+                               customer_lst=customer_values_sample,
+                               selected_cust_id=data["cust_id"],
+                               selected_from_date=data["from_date"],
+                               selected_to_date=data["to_date"],
+                               table_cols=table_cols,
+                               agronomists=results)
 
 
-@app.route('/request3_handled', methods=['POST'])
-def request3_handle():
-    # Get a data in json format(similar)
-    data = request.form
 
-    statement = text("""
-        SELECT agronomist_id FROM
-        (SELECT agronomist_id, COUNT(*) AS num FROM 
-        Degustation INNER JOIN Degustation_Customer ON 
-        Degustation.degustation_id = Degustation_Customer.degustation_id 
-        WHERE Degustation_Customer.customer_id = :cus_id GROUP BY agronomist_id) WHERE num > :n_times AND degustation_date 
-        BETWEEN :date_start AND :date_end;
-        """)
+@app.route('/request4', methods=['GET', 'POST'])
+def request_page_action4():
+    statement = text("""SELECT agronomist_id, agronomist_name FROM agronomist""")
 
-    rs = db.session.execute(statement, **data).all()
+    agronomists_list = db.session.execute(statement).all()
     db.session.commit()
     db.session.close()
 
-    print("request3_handle -- ", rs)
+    # print("request_page_action2", customer_values_sample)
 
-    # TODO print all agronomes
-    return 'Hello3'
+    table_cols = ["agronomist_id", "agronomist_name"]
+    if request.method == 'GET':
+        return render_template("request4.html",
+                               agronomists_list=agronomists_list,
+                               selected_agronomist_id=1,
+                               selected_from_date="-",
+                               selected_to_date="-",
+                               table_cols=table_cols,
+                               agronomists=[(0, "No result")])
+
+    if request.method == 'POST':
+        # Get a data in json format(similar)
+        data = {
+            "agronomist_id": int(request.form.get("agronomist_id")),
+            "from_date": request.form.get("from_date"),
+            "to_date": request.form.get("to_date")
+        }
+
+        today = date.today()
+        if data["from_date"] == '':
+            data["from_date"] = '2010-01-01'
+
+        if data["to_date"] == '':
+            data["to_date"] = today.strftime("%Y-%m-%d")
+
+        print("data in request4_handle -- ", data)
+
+        statement = text(
+            """
+            SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN 
+(SELECT DISTINCT agronomist_id FROM Trip_Agronomist WHERE agronomist_id != :agronomist_id AND trip_id IN
+(SELECT trip_id FROM business_trip WHERE trip_date BETWEEN :from_date AND :to_date AND trip_id IN 
+(SELECT trip_id FROM Trip_Agronomist WHERE agronomist_id = :agronomist_id)));
+            """
+        )
+
+        rs = db.session.execute(statement, {
+            "agronomist_id": data["agronomist_id"],
+            "from_date": data["from_date"],
+            "to_date": data["to_date"]
+        })
+        db.session.commit()
+        db.session.close()
+
+        results = rs.fetchall()
+        print("request4_handle results -- ", results)
+
+        return render_template("request4.html",
+                               agronomists_list=agronomists_list,
+                               selected_cust_id=data["agronomist_id"],
+                               selected_from_date=data["from_date"],
+                               selected_to_date=data["to_date"],
+                               table_cols=table_cols,
+                               agronomists=results)
+
+
+
+@app.route('/request5', methods=['GET', 'POST'])
+def request_page_action5():
+    statement = text("""SELECT customer_id, customer_name FROM customer""")
+
+    customer_values_sample = db.session.execute(statement).all()
+    db.session.commit()
+    db.session.close()
+
+    print("request_page_action5", customer_values_sample)
+
+    table_cols = ["agronomist_id", "agronomist_name"]
+    if request.method == 'GET':
+        return render_template("request5.html",
+                               customer_lst=customer_values_sample,
+                               selected_cust_id=1,
+                               selected_from_date="-",
+                               selected_to_date="-",
+                               table_cols=table_cols,
+                               agronomists=[(0, "No result")])
+
+    if request.method == 'POST':
+        # Get a data in json format(similar)
+        data = {
+            "cust_id": int(request.form.get("cust_id")),
+            "from_date": request.form.get("from_date"),
+            "to_date": request.form.get("to_date")
+        }
+
+        today = date.today()
+        if data["from_date"] == '':
+            data["from_date"] = '2010-01-01'
+
+        if data["to_date"] == '':
+            data["to_date"] = today.strftime("%Y-%m-%d")
+
+        print("data in request5_handle -- ", data)
+
+        statement = text(
+            """
+            SELECT agronomist_name FROM Agronomist WHERE agronomist_id IN 
+(SELECT agronomist_id FROM Ordering WHERE customer_id = :cust_id AND order_date BETWEEN :from_date AND :to_date )
+AND agronomist_id IN
+(SELECT agronomist_id FROM Degustation WHERE degustation_id IN 
+(SELECT * FROM degustation_customer WHERE customer_id = :cust_id)
+ AND degustation_date BETWEEN :from_date AND :to_date);
+            """
+        )
+
+        rs = db.session.execute(statement, {
+            "cust_id": data["cust_id"],
+            "from_date": data["from_date"],
+            "to_date": data["to_date"]
+        })
+        db.session.commit()
+        db.session.close()
+
+        results = rs.fetchall()
+        print("request5_handle results -- ", results)
+
+        return render_template("request5.html",
+                               customer_lst=customer_values_sample,
+                               selected_cust_id=data["cust_id"],
+                               selected_from_date=data["from_date"],
+                               selected_to_date=data["to_date"],
+                               table_cols=table_cols,
+                               agronomists=results)
+
+
+@app.route('/request6', methods=['GET', 'POST'])
+def request_page_action6():
+    statement = text("""SELECT product_id FROM product""")
+
+    products_num = len(db.session.execute(statement).all())
+    db.session.commit()
+    db.session.close()
+
+    table_cols = ["customer_id", "customer_name"]
+    if request.method == 'GET':
+        return render_template("request1.html",
+                               products_num=products_num,
+                               selected_from_date="-",
+                               selected_to_date="-",
+                               sold_times=1,
+                               table_cols=table_cols,
+                               customers=[(0, "No result")])
+
+    if request.method == 'POST':
+        # Get a data in json format(similar)
+        data = {
+            "products_num": int(request.form.get("products_num")),
+            "from_date": request.form.get("from_date"),
+            "to_date": request.form.get("to_date")
+        }
+
+        today = date.today()
+        if data["from_date"] == '':
+            data["from_date"] = '2013-01-01'
+
+        if data["to_date"] == '':
+            data["to_date"] = today.strftime("%Y-%m-%d")
+
+        print("data in request6_handle -- ", data)
+
+        statement = text(
+            """
+            SELECT customer_name FROM Customer WHERE customer_id IN
+(SELECT customer_id FROM 
+ (SELECT customer_id, COUNT(DISTINCT product_id) AS num FROM Ordering WHERE order_date BETWEEN :from_date AND :to_date GROUP BY customer_id) h
+ WHERE h.num > :products_num);
+            """
+        )
+
+        rs = db.session.execute(statement, {
+            "products_num": data["products_num"],
+            "from_date": data["from_date"],
+            "to_date": data["to_date"]
+        })
+        db.session.commit()
+        db.session.close()
+
+        results = rs.fetchall()
+        print("request6_handle -- ", results)
+
+        return render_template("request6.html",
+                               products_num=data["products_num"],
+                               selected_from_date=data["from_date"],
+                               selected_to_date=data["to_date"],
+                               table_cols=table_cols,
+                               customers=results)
 
 
 if __name__ == '__main__':
